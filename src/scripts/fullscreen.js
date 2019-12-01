e4afde0669a3039a99c7bb4e74da3b4011c4aef3
peterpeterparker <david.dalbusco@outlook.com>
2019-12-01 16:20:34 +0100

    feat: hide pager in fullscreen on mouse inactivity
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- src/scripts/fullscreen.js
+++ src/scripts/fullscreen.js
@@ -11,6 +11,16 @@ initFullscreen = () => {
             if ($event && navigation) {
                 navigation.style.visibility = $event.detail ? 'inherit' : 'hidden';
             }
+
+            const deck = document.getElementById('slider');
+
+            if ($event && deck) {
+                if ($event.detail) {
+                    deck.style.removeProperty('--pager-display');
+                } else {
+                    deck.style.setProperty('--pager-display', 'none');
+                }
+            }
         }, {passive: true});
 
         resolve();
