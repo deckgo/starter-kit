710f664ec475fec51959ba3084fa535901419145
peterpeterparker <david.dalbusco@outlook.com>
2020-04-13 08:51:41 +0200

    feat: window not needed
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ import { defineCustomElements as webSocialShareElements } from 'web-social-share
 deckDeckGoElements().then(async () => {
     await deckDeckGoChartsElements();
     await deckDeckGoQRCodeElements();
-    await deckDeckGoHighlightCodeElements(window);
+    await deckDeckGoHighlightCodeElements();
     await deckDeckGoLazyImgElements();
     await deckDeckGoSocialElements();
     await deckDeckGoYoutubeElements();
