33075118c58364013d9b3b829557b9f534a38a79
peterpeterparker <david.dalbusco@outlook.com>
2020-08-28 13:40:11 +0200

    fix: local Google Fonts not loaded

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ const config = {
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
     }
